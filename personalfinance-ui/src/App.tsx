7b9b58ccdc8f947fd145d667acba0a9caa22fec8
vbirb <vbirbili@onemagnify.com>
2022-11-30 15:26:32 -0500

    Added the REact App

---
+++ personalfinance-ui/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Button, Header, List } from 'semantic-ui-react'
+
+function App() {
+const [activities, setActivities] = useState([]);
+
+useEffect(() =>{
+  axios.get('http://localhost:5000/api/activities').then (response => {
+    //console.log(response);
+    setActivities(response.data);
+  })
+},[]);
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+
+         <List>
+         {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}x
+            </List.Item>
+          ))}
+         </List>
+        <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  );
+}
+
+export default App;
