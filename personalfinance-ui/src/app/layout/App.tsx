9439fa888f39853f7a530a6995b86123b57d40e6
vbirb <vbirbili@onemagnify.com>
2022-12-05 22:15:09 -0500

    update section 6

--- personalfinance-ui/src/app/layout/App.tsx
+++ personalfinance-ui/src/app/layout/App.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment, useEffect, useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,13 +15,23 @@ function App() {
   >(undefined);
   const [editMode, setEditMode] = useState(false);
 
+  const [loading, setLoading] = useState(true);
+
+  const [submitting, setSubmitting] = useState(false);
+
   useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-      .then((response) => {
-        //console.log(response);
-        setActivities(response.data);
+    // axios.get<Activity[]>("http://localhost:5000/api/activities")
+    agent.Activities.list().then((response) => {
+      //console.log(response);
+      // workaround for Dates
+      let activities: Activity[] = [];
+      response.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        activities.push(activity);
       });
+      setActivities(activities);
+      setLoading(false);
+    });
   }, []);
 
   function handleSelectActivity(id: string) {
@@ -38,19 +50,39 @@ function App() {
     setEditMode(false);
   }
   function handleCreateOrEditActivity(activity: Activity) {
-    activity.id
-      ? setActivities([
+    setSubmitting(true);
+
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([
           ...activities.filter((x) => x.id !== activity.id),
           activity,
-        ])
-      : setActivities([...activities, { ...activity, id: uuid() }]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      });
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      });
+    }
   }
 
-  function handleDeleteActivity(id: string){
-  setActivities([...activities.filter(x => x.id !== id)])
-}
+  function handleDeleteActivity(id: string) {
+    setSubmitting(true);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter((x) => x.id !== id)]);
+
+      setSubmitting(false);
+    });
+  }
+
+  if (loading) return <LoadingComponent content="loading app" />;
 
   return (
     <Fragment>
@@ -65,7 +97,8 @@ function App() {
           openForm={handleFormOpen}
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
-          deleteActivity = {handleDeleteActivity}
+          deleteActivity={handleDeleteActivity}
+          submitting={submitting}
         />
       </Container>
     </Fragment>
--- personalfinance-ui/src/features/activities/dashboard/ActivityList.tsx
+++ personalfinance-ui/src/features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { Item, Segment, Button, Label } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { useState } from "react";
 
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
-export default function ActivityList({ activities, selectActivity , deleteActivity}: Props) {
+export default function ActivityList({
+  activities,
+  selectActivity,
+  deleteActivity,
+  submitting,
+}: Props) {
+  const [target, setTarget] = useState("");
+
+  function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
       <Item.Group divided>
@@ -23,8 +37,20 @@ export default function ActivityList({ activities, selectActivity , deleteActivi
                 </div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={() => selectActivity(activity.id)} floated="right" content="view" color="blue" />
-                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="delete" color="red" />
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated="right"
+                  content="view"
+                  color="blue"
+                />
+                <Button
+                  name={activity.id}
+                  loading={submitting && target === activity.id}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
+                  floated="right"
+                  content="delete"
+                  color="red"
+                />
                 <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
