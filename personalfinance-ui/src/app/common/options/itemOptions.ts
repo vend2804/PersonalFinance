d5d88855147d5741b1b0f0dcbd5d2026178c9af3
vbirb <vbirbili@onemagnify.com>
2022-12-28 22:39:33 -0500

    update the Items

--- personalfinance-ui/src/app/common/options/itemOptions.ts
+++ personalfinance-ui/src/app/common/options/itemOptions.ts
@@ -1,6 +1,4 @@
-
 export const itemdOptions = [
-    { text: "Payroll", value: 3 },
-    { text: "Bonus", value: 4 },
-    { text: "Misc", value: 5 },
-  ];
+  { text: "Paycheck", value: 1 },
+  { text: "Bonus", value: 2 },
+];
